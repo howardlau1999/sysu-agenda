7db55fa25c5c18ffe816b5d44b7435881c8a3416
Howard Lau <howardlau1999@hotmail.com>
2018-10-20 11:36:11 +0800

    merge

--- test.cpp
+++ test.cpp
@@ -4,4 +4,9 @@
 TEST(Date, convert) {
     Date date("2018-09-10/12:00");
     EXPECT_EQ(date, Date(2018, 9, 10, 12, 00));
+    EXPECT_EQ(Date::stringToDate("2018-09-10/12:00"),
+              Date(2018, 9, 10, 12, 00));
+    EXPECT_EQ(Date(0, 0, 0, 0, 0), Date::stringToDate("2018-9-10/12:00"));
+    EXPECT_EQ(Date(0, 0, 0, 0, 0), Date::stringToDate("2018/9/10/12:00"));
+    EXPECT_EQ(Date(0, 0, 0, 0, 0), Date::stringToDate("1018/9/10/12:00"));
 }
